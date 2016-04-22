1190b508e29e658057d9a7376268c3e808d79a36
lidan <lidan11923@sina.cn>
2016-04-22 14:10:41 +0800

    Initial commit

---
+++ star-dreaming-service/src/main/java/com/sky/istar/UserService.java
@@ -0,0 +1,10 @@
+package com.sky.istar;
+
+/**
+ * Created by ld on 2016/4/22.
+ */
+public class UserService {
+    public static void main (String[] args){
+        System.out.print("hhhhh");
+    }
+}
